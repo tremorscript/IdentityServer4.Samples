12c16366b55e0878fba146db6df678251ceffd5e
tremorScript <tremorscript@gmail.com>
2023-03-09 13:41:05 +0530

    Test changes

---
+++ 2_AddingUI/MVCApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVCApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
